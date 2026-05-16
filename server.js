91e9ec0d35e84fadcf61e1f76393f5030d12ba34
Ryan <ryan@example.com>
2026-05-16 13:20:03 -0300

    Fix catch-all route for Express 5

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get('/search', async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
 
